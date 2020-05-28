42d162057d44ee4898f094c97c1c76199327d314
Shobhit Singh <Shobhit5@hotmail.com>
2020-05-28 11:29:41 +0200

    local image add

--- src/Component/View/Details.js
+++ src/Component/View/Details.js
@@ -1,11 +1,12 @@
 import React from "react";
+import ppfgirls from "../../logo.jpg";
 
 function Details(props) {
   const imageUrl = props.image ? props.image.medium : "./temp";
   return (
     <div className="box">
       <div>
-        <img src={imageUrl} className="show-title-image" alt={props.imgAlt} />
+        <img src={ppfgirls} className="show-title-image" alt={props.imgAlt} />
       </div>
       <div className="show-details">
         <div
